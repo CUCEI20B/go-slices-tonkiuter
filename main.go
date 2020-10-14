0c478bd4724547725e9e4b10522ac61a8cf4d1f5
Alvaro Felix <alvaro.felix@alumnos.udg.mx>
2020-10-14 02:21:22 -0500

    Se crearon los ciclos y el slice donde estan almacenados los datos

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var s []int
+	var n int
+	var k int
+	var suma int
+
+	//fmt.Println("Tamaño del Slice: ")
+	fmt.Scanln(&n)
+
+	for i:= 0; i < n; i++ {
+		//fmt.Println("Ingresa un numero: ")
+		fmt.Scanln(&k)
+		s = append(s,k)
+	}
+
+	//fmt.Println("Suma de Slide: ")
+	
+	for j:= 0; j < n; j++{
+		suma += s[j]
+	}
+	
+	fmt.Println(suma)
+
 }
